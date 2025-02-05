ff07b71322415a9ae46b4903ff95bb7241a6143a
pardeep-pathania <pathanianikhil145@gmail.com>
2025-02-05 19:40:50 +0530

    add song folder and give them a frame like card

--- spotify.js
+++ spotify.js
@@ -1,1 +1,55 @@
 
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/song/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split('/').pop())
+        }
+
+    }
+    return songs;
+}
+async function main() {
+
+    // Get the list of all songs 
+
+    let songs = await getSongs()
+
+    console.log(songs)
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+                            <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", "")}</div>
+                                <div>Pathania Band</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div>
+        </li>`;
+    }
+
+    // Play the first song
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+
+        // The duration variable now holds the duration (in seconds) of the audio clip
+    });
+}
+main()
